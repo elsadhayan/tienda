21ed1880555fcbe6d3888a1cde718f8111bc8a5c
andy46473@gmail.com <andy46473@gmail.com>
2026-05-26 07:39:34 -0600

    cambios

--- Downloads/900angular/tiendita/src/app/app.routes.ts
+++ Downloads/900angular/tiendita/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
 
 
 export const routes: Routes = [
@@ -7,6 +8,10 @@ export const routes: Routes = [
   path:'home',component:(Home)
 
 },
+  {
+    path: 'products',
+    component: Products
+  },
 {
   path:'',redirectTo:'home',pathMatch:'full'
 }
